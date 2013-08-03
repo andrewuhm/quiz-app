d400fa7a505c12a4fe41fbf8aac82c7dd411c669
Andrew Uhm <andrew.uhm@nfl.com>
2013-08-03 16:26:57 -0700

    Can submit quizzes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
 				uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+	has_surveys
 
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
