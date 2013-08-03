e1ecc102554605c6278affeb6af663bfcbdbee41
Andrew Uhm <andrew.uhm@nfl.com>
2013-08-02 17:21:00 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      sign_in @user
   		flash[:success] = "Account created.  Welcome to the Study Guide Quiz Application"
   		redirect_to @user
   	else 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 QuizApp::Application.routes.draw do
-
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
 
-  match '/help',  to: 'static_pages#help',  via: 'get'
-  match '/signup',  to: 'users#new',        via: 'get'
+  match '/help',      to: 'static_pages#help',    via: 'get'
+  match '/signup',    to: 'users#new',            via: 'get'
+  match '/signin',    to: 'sessions#new',         via: 'get'
+  match '/signout',   to: 'sessions#destroy',     via: 'delete'
+
   namespace :quizzes do
     resources :surveys
     resources :attempts, :only => [:new, :create]
